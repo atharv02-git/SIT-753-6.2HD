ef4ac90ac84f00a39b06765269f66bc13941e712
Atharv Bhandare <atharvbhandare2001@gmail.com>
2024-04-16 13:25:20 +1000

    MVC Arhcitecture server.js

--- server.js
+++ server.js
@@ -8,60 +8,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 app.use('/api/cats', router);
 
-// // database connectivity
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-
-// const uri = "mongodb+srv://atharv02:Atharv2@cardcluster.zwt8hfn.mongodb.net/?retryWrites=true&w=majority&appName=cardCluster";
-
-// const client = new MongoClient(uri, {
-//     serverApi: {
-//       version: ServerApiVersion.v1,
-//       strict: true,
-//       deprecationErrors: true,
-//     }
-// });
-
-// async function run() {
-//     try {
-//       await client.connect();
-//       await client.db("admin").command({ ping: 1 });
-//       console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//     } finally {
-//       await client.close();
-//     }
-// }
-
-
-
-// app.get('/', (req, res) => {
-//     res.render('index.html');
-// });
-
-// app.post('/api/cat', async (req, res) => {
-//     let cat = req.body;
-//     let result = await postCat(cat);
-//     client.close();
-//     res.json({statusCode: 201, message: 'success', data: result});
-// });
-
-// async function postCat(cat) {
-//     await client.connect();
-//     let collection = await client.db().collection('Cats');
-//     return collection.insertOne(cat);
-// }
-
-// app.get('/api/cats', async (req, res) => {
-//     let result = await getAllCats();
-//     client.close();
-//     res.json({statusCode: 201, message: 'success', data: result});
-// });
-
-// async function getAllCats() {
-//     await client.connect();
-//     let collection = await client.db().collection('Cats');
-//     return collection.find().toArray();
-// }
-
 
 app.listen(port, () => {
     console.log("App is listeneing on port: " + port)
